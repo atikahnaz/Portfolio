beab0b7a75cc09c8956c8ad59093b5a22ce53dfc
Atikah <atikahnazir6@gmail.com>
2024-02-22 16:40:10 +0800

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    colors: {
+      "pf-cream": "#FBFAF2",
+      "pf-light-blue": "#4979F5",
+      "pf-blue": "#102E69",
+      "pf-yellow": "#FFF61E",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
