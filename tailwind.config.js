5a7b2a16cbfae8298462e6714497ad22f20e2eb1
Atikah <atikahnazir6@gmail.com>
2024-02-27 15:15:16 +0800

    header hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    colors: {
-      "pf-cream": "#FBFAF2",
-      "pf-light-blue": "#4979F5",
-      "pf-blue": "#102E69",
-      "pf-yellow": "#FFF61E",
+    extend: {
+      colors: {
+        "pf-cream": "#FBFAF2",
+        "pf-light-blue": "#4979F5",
+        "pf-blue": "#102E69",
+        "pf-yellow": "#FFF61E",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
