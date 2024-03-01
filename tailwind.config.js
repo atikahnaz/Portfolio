32e9972978d86d6706433c8827879d285fd314bb
Atikah <atikahnazir6@gmail.com>
2024-03-01 14:08:14 +0800

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
         "pf-light-blue": "#4979F5",
         "pf-blue": "#102E69",
         "pf-yellow": "#FFF61E",
+        "pf-numbers": "#748CBA",
+        "pf-title": "#2D55A2",
       },
       fontFamily: {
         customFont: ["Roboto Serif", "sans-serif"],
