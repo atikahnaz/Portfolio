1f2f679cce522e5e514a97b1e8e710105e87596a
Atikah <atikahnazir6@gmail.com>
2024-05-30 17:06:48 +0800

    animate hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,28 @@ module.exports = {
       fontFamily: {
         customFont: ["Roboto Serif", "sans-serif"],
       },
+      keyframes: {
+        "border-spin": {
+          "100%": {
+            transform: "rotate(-360deg)",
+          },
+        },
+        "fade-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(30px) scale(0.9)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0px) scale(1)",
+          },
+        },
+      },
+      animation: {
+        "border-spin": "border-spin 7s linear infinite",
+        "fade-text": "fade-up 1s",
+        "fade-letter": "fade-up 1.5s",
+      },
     },
   },
   plugins: [],
